86e086fcb8414395a94feba7b0e716006f8687cd
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-13 16:50:41 -0700

    Take into account scrollbar width.
    Fixes #2.

--- menu/menu.go
+++ menu/menu.go
@@ -30,6 +30,12 @@ func (rm ReactionsMenu) Show(this dom.HTMLElement, commentID uint64) {
 
 	Reactions.menu.Style().SetProperty("display", "initial", "")
 
+	// Reactions.menu aims to have 270px client width. Due to optional scrollbars
+	// taking up some of that space, we may need to compensate and increase width.
+	if scrollbarWidth := Reactions.results.OffsetWidth() - Reactions.results.Get("clientWidth").Float(); scrollbarWidth > 0 {
+		Reactions.menu.Style().SetProperty("width", fmt.Sprintf("%fpx", 270+scrollbarWidth+1), "")
+	}
+
 	top := this.GetBoundingClientRect().Top - Reactions.menu.GetBoundingClientRect().Height - 8
 	if top < 10 {
 		top = 10
@@ -41,8 +47,9 @@ func (rm ReactionsMenu) Show(this dom.HTMLElement, commentID uint64) {
 }
 
 type ReactionsMenu struct {
-	menu   *dom.HTMLDivElement
-	filter *dom.HTMLInputElement
+	menu    *dom.HTMLDivElement
+	filter  *dom.HTMLInputElement
+	results *dom.HTMLDivElement
 
 	authenticatedUser bool
 }
@@ -70,22 +77,39 @@ func setupReactionsMenu() {
 	Reactions.filter.SetClass("rm-reactions-filter")
 	Reactions.filter.Placeholder = "Search"
 	container.AppendChild(Reactions.filter)
-	results := document.CreateElement("div").(*dom.HTMLDivElement)
-	results.SetClass("rm-reactions-results")
-	container.AppendChild(results)
+	Reactions.results = document.CreateElement("div").(*dom.HTMLDivElement)
+	Reactions.results.SetClass("rm-reactions-results")
+	Reactions.results.AddEventListener("click", false, func(event dom.Event) {
+		me := event.(*dom.MouseEvent)
+		x := (me.ClientX - int(Reactions.results.GetBoundingClientRect().Left) + Reactions.results.Underlying().Get("scrollLeft").Int()) / 30
+		if x >= 9 {
+			return // Out of bounds to the right, likely because of scrollbar.
+		}
+		y := (me.ClientY - int(Reactions.results.GetBoundingClientRect().Top) + Reactions.results.Underlying().Get("scrollTop").Int()) / 30
+		i := y*9 + x
+		if i < 0 || i >= len(filtered) {
+			return
+		}
+		emojiID := filtered[i]
+		fmt.Printf("clicked %q reaction\n", emojiID)
+	})
+	container.AppendChild(Reactions.results)
 	preview := document.CreateElement("div").(*dom.HTMLDivElement)
 	container.AppendChild(preview)
 	preview.SetOuterHTML(`<div class="rm-reactions-preview"><span id="rm-reactions-preview-emoji"></span><span id="rm-reactions-preview-label"></span></div>`)
 
-	updateFilteredResults(Reactions.filter, results)
+	updateFilteredResults(Reactions.filter, Reactions.results)
 	Reactions.filter.AddEventListener("input", false, func(dom.Event) {
-		updateFilteredResults(Reactions.filter, results)
+		updateFilteredResults(Reactions.filter, Reactions.results)
 	})
 
-	results.AddEventListener("mousemove", false, func(event dom.Event) {
+	Reactions.results.AddEventListener("mousemove", false, func(event dom.Event) {
 		me := event.(*dom.MouseEvent)
-		x := (me.ClientX - int(results.GetBoundingClientRect().Left) + results.Underlying().Get("scrollLeft").Int()) / 30
-		y := (me.ClientY - int(results.GetBoundingClientRect().Top) + results.Underlying().Get("scrollTop").Int()) / 30
+		x := (me.ClientX - int(Reactions.results.GetBoundingClientRect().Left) + Reactions.results.Underlying().Get("scrollLeft").Int()) / 30
+		if x >= 9 {
+			return // Out of bounds to the right, likely because of scrollbar.
+		}
+		y := (me.ClientY - int(Reactions.results.GetBoundingClientRect().Top) + Reactions.results.Underlying().Get("scrollTop").Int()) / 30
 		i := y*9 + x
 		updateSelected(i)
 	})
